96a371df52a089b6b567cac6416328d5954884aa
C-DGameMaker <charlotte5634@outlook.com>
2026-09-15 15:47:54 -0300

    Started Turn Based system

--- Assets/TurnBasedCombat.cs
+++ Assets/TurnBasedCombat.cs
@@ -1,16 +1,48 @@
+using TMPro;
 using UnityEngine;
 
 public class TurnBasedCombat : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] int playerCurrentHealth;
+    [SerializeField] int playerMaxHealth;
+
+    [SerializeField] int enemyCurrentHealth;
+    [SerializeField] int enemyMaxHealth;
+
+    [SerializeField] TextMeshProUGUI playerHealthString;
+    [SerializeField] TextMeshProUGUI enemyHealthString;
+
+    [SerializeField] GameObject player;
+    [SerializeField] Transform playerStartLocation;
+    [SerializeField] GameObject enemy;
+    [SerializeField] Transform enemyStartLocation;
+
+    [SerializeField] int playerAttack;
+    [SerializeField] int enemyAttack;
+
+    private bool isPlayerTurn;
+
+    private void Start()
     {
-        
+        playerCurrentHealth = playerMaxHealth;
+        enemyCurrentHealth = enemyMaxHealth;
+
+        playerStartLocation = player.transform;
+        enemyStartLocation = enemy.transform;
+
+        HealthTextUpdate();
+
+        isPlayerTurn = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HealthTextUpdate()
     {
-        
+        playerHealthString.text = playerCurrentHealth.ToString() + " / " + playerMaxHealth.ToString();
+        enemyHealthString.text = enemyCurrentHealth.ToString() + " / " + enemyMaxHealth.ToString();
+    }
+
+    private void ChangeTurn()
+    {
+        isPlayerTurn = !isPlayerTurn;
     }
 }
